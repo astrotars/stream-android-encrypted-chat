05d0422beaf7c9add67bc2186c72504d472556a6
Jeff Taggart <jeff@jetaggart.com>
2020-03-16 16:02:49 -0600

    Log in with backend

--- android/app/src/main/java/io/getstream/encryptedchat/ChannelsActivity.kt
+++ android/app/src/main/java/io/getstream/encryptedchat/ChannelsActivity.kt
@@ -6,39 +6,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
-import com.getstream.sdk.chat.StreamChat
 import com.getstream.sdk.chat.enums.Filters.*
-import com.getstream.sdk.chat.model.Channel
-import com.getstream.sdk.chat.rest.User
 import com.getstream.sdk.chat.viewmodel.ChannelListViewModel
 import io.getstream.encryptedchat.databinding.ActivityChannelsBinding
-import okhttp3.MediaType
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
-import java.util.*
 
 class ChannelsActivity : AppCompatActivity() {
-  val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
-  var client: OkHttpClient = OkHttpClient()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    // setup the client using the example API key
-    // normally you would call init in your Application class and not the activity
-    StreamChat.init("whe3wer2pf4r", this.applicationContext)
-    val client = StreamChat.getInstance(this.application)
-    val extraData = HashMap<String, Any>()
-    extraData["name"] = "John Smith"
-    val currentUser = User("john", extraData)
-    // User token is typically provided by your server when the user authenticates
-    client.setUser(
-      currentUser,
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiam9obiJ9.mIGaHTUCpX3BwcH1taQMpPld6j0m6iFNHjrvcb0puZs"
-    )
 
     // we're using data binding in this example
     val binding: ActivityChannelsBinding =
@@ -53,7 +28,7 @@ class ChannelsActivity : AppCompatActivity() {
     binding.channelList.setViewHolderFactory(EncryptedMessageChannelViewHolderFactory())
 
     // query all channels of type messaging
-    val filter = and(eq("type", "messaging"), `in`("members", currentUser.id))
+    val filter = and(eq("type", "messaging"), `in`("members", intent.getStringExtra(EXTRA_USER)))
     viewModel.setChannelFilter(filter)
 
     // click handlers for clicking a user avatar or channel
@@ -63,23 +38,14 @@ class ChannelsActivity : AppCompatActivity() {
     }
   }
 
-  fun post(url: String, json: String): String {
-    val body: RequestBody = json.toRequestBody(JSON)
-    val request: Request = Request.Builder()
-      .url(url)
-      .post(body)
-      .build()
-    client.newCall(request).execute().use {
-      return it.body!!.string()
-    }
-  }
-
   companion object {
     private val EXTRA_USER = "io.getstream.encryptedchat.USER"
+    private val EXTRA_VIRGIL_TOKEN = "io.getstream.encryptedchat.VIRGIL_TOKEN"
 
-    fun newIntent(context: Context, user: String): Intent {
+    fun newIntent(context: Context, user: String, virgilToken: String): Intent {
       val intent = Intent(context, ChannelsActivity::class.java)
       intent.putExtra(EXTRA_USER, user)
+      intent.putExtra(EXTRA_VIRGIL_TOKEN, virgilToken)
       return intent
     }
   }
--- android/app/src/main/java/io/getstream/encryptedchat/MainActivity.kt
+++ android/app/src/main/java/io/getstream/encryptedchat/MainActivity.kt
@@ -3,10 +3,23 @@ package io.getstream.encryptedchat
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.getstream.sdk.chat.StreamChat
+import com.getstream.sdk.chat.rest.User
 import io.getstream.encryptedchat.databinding.ActivityMainBinding
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
+  val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
+  var client: OkHttpClient = OkHttpClient()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
@@ -15,14 +28,58 @@ class MainActivity : AppCompatActivity() {
       DataBindingUtil.setContentView(this, R.layout.activity_main)
     // Specify the current activity as the lifecycle owner.
     binding.lifecycleOwner = this
-    binding.username.setOnClickListener {
-    }
 
     binding.submit.setOnClickListener {
-      val intent = ChannelsActivity.newIntent(this, "jeff")
-      startActivity(intent)
+      val user: String = binding.user.text.toString()
 
+      doAsync {
+        val authToken = signIn(user)
+        val streamToken = getStreamToken(authToken)
+        val virgilToken = getVirgilToken(authToken)
+
+        uiThread { context ->
+          initStream(user, streamToken)
+          val intent = ChannelsActivity.newIntent(context, user, virgilToken)
+          startActivity(intent)
+        }
+      }
+    }
+  }
+
+  private fun signIn(user: String): String {
+    val response = post("/v1/authenticate", mapOf("user" to user))
+    return response.getString("authToken")
+  }
+
+  private fun getVirgilToken(authToken: String): String {
+    val response = post("/v1/virgil-credentials", mapOf(), authToken)
+    return response.getString("token")
+  }
+
+  private fun getStreamToken(authToken: String): String {
+    val response = post("/v1/stream-credentials", mapOf(), authToken)
+    return response.getString("token")
+  }
+
+  private fun initStream(user: String, token: String) {
+    StreamChat.init("whe3wer2pf4r", this.applicationContext)
+    val client = StreamChat.getInstance(this.application)
+    val currentUser = User(user, hashMapOf<String, Any>("name" to user))
+    client.setUser(currentUser, token)
+  }
+
+  private fun post(path: String, body: Map<String, Any>, authToken: String? = null): JSONObject {
+    val request = Request.Builder()
+      .url("https://96084840.ngrok.io${path}")
+      .post(JSONObject(body).toString().toRequestBody(JSON))
+
+    if (authToken != null) {
+      request.addHeader("Authorization", "Bearer $authToken")
     }
 
+    client.newCall(request.build()).execute().use {
+      return JSONObject(it.body!!.string())
+    }
   }
+
 }
